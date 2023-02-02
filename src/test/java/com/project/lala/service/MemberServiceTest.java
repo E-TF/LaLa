6d2380a237d405d333bc4afebfb41415fc88c892
tpleehan <tpleehan@gmail.com>
2023-02-03 03:34:31 +0900

    Test: 테스트 코드 추가 - Member Register 관련 Tdd 추가

--- src/test/java/com/project/lala/service/MemberServiceTest.java
+++ src/test/java/com/project/lala/service/MemberServiceTest.java
@@ -1,10 +1,13 @@
 package com.project.lala.service;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Optional;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
@@ -15,7 +18,7 @@ import com.project.lala.dto.SignUpRequest;
 import com.project.lala.entity.Member;
 import com.project.lala.repository.MemberRepository;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MemberServiceTest {
 
 	private Member member;
@@ -42,4 +45,45 @@ class MemberServiceTest {
 		assertThat(findMemberId.get().getId()).isEqualTo(joinMember.getId());
 		assertThat(findMemberId.get().getEmail()).isEqualTo(joinMember.getEmail());
 	}
+
+	@Test
+	@DisplayName("이미 같은 ID가 존재하는 경우 가입 실패")
+	public void dupIdExistsFail() {
+		Member savedResult = memberRepository.save(testBuilderId());
+
+		Optional<Member> findMemberId = memberRepository.findById(savedResult.getId());
+
+		assertThat(findMemberId).isNotNull();
+		assertThat(findMemberId.get().getId()).isNotNull();
+		assertThat(findMemberId.get().getId()).isEqualTo(savedResult.getId());
+	}
+
+	@Test
+	@DisplayName("중복된 아이디가 없는 경우 가입 성공")
+	public void dupIdExistsSuccess() {
+		memberRepository.save(testBuilderId());
+
+		Optional<Member> savedMember = memberRepository.findById(1L);
+		assertEquals(1L, savedMember.get().getId());
+		assertEquals("test_email@email.test", savedMember.get().getEmail());
+	}
+
+	@Test
+	@DisplayName("가입 후 메일전송")
+	public void signUpThenSendMail() {
+		memberRepository.save(testBuilderId());
+		ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+
+	}
+
+	private Member testBuilderId() {
+		return Member.builder()
+			.id(1L)
+			.loginId("test_id")
+			.email("test_email@email.test")
+			.name("test_name")
+			.password(encryptionService.encrypt("!@#test123"))
+			.build();
+	}
+
 }
