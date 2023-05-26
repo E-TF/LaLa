a3f20c69b81a9dab85635b446bb05720a813fd68
tpleehan <tpleehan@gmail.com>
2023-05-26 22:49:46 +0900

    fix: 회원 탈퇴 기능 수정 #9 - url 노출에 대한 보안 취약점 수정

--- src/main/java/com/project/lala/controller/MemberController.java
+++ src/main/java/com/project/lala/controller/MemberController.java
@@ -1,10 +1,12 @@
 package com.project.lala.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.lala.entity.Member;
 import com.project.lala.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
@@ -17,9 +19,11 @@ public class MemberController {
 
 	private final MemberService memberService;
 
-	@DeleteMapping("/members/{id}")
-	public ResponseEntity<Void> deleteMember(@PathVariable Long id) {
-		memberService.deleteMember(id);
+	@DeleteMapping("/members")
+	public ResponseEntity<Void> deleteMember(HttpSession session) {
+		Member sessionMemberId = (Member)session.getAttribute("memberId");
+		Long memberId = sessionMemberId.getId();
+		memberService.deleteMember(memberId);
 		return ResponseEntity.noContent().build();
 	}
 
